704ca07fa35a63d210260aff81488d2039f18683
ANaddafi <alireza.na81@gmail.com>
2021-02-09 02:06:06 +0330

    feat: ships, points, map

--- structs.h
+++ structs.h
@@ -1,4 +1,5 @@
 #define STRUCT
+#define LEN 10
 
 struct User{
     char user_name[100];
@@ -6,8 +7,23 @@ struct User{
     int current_score;
 };
 
-struct Map{
+struct point
+{
+    int col;
+    char row;
+};
 
+struct Ship
+{
+    struct point top, back;
+    int exploded, lenght;
+    struct Ship *next;
+};
+
+struct Map{
+    struct Ship* ships_head;
+    char view[LEN][LEN];
+    char leaked[LEN][LEN];
 };
 
 struct GameData{
@@ -18,6 +34,8 @@ struct GameData{
     int finished;
 };
 
+/////////////////////////////////
+/// functions
 
 void print_user_full(struct User user)
 {
@@ -31,4 +49,42 @@ void print_user_full(struct User user)
 void print_user(struct User user)
 {
     printf("%s \t %d\n", user.user_name, user.total_score);
+}
+
+void view_map(struct Map mp)
+{
+    int i, j;
+
+    printf(" ");
+    for(i = 1; i <= LEN; i++)
+        printf(" %d", i);
+    printf("\n");
+
+    for(i = 0; i < LEN; i++){
+        printf("%c ", i+'a');
+
+        for(j = 0; j < LEN; j++)
+            printf("%c ", mp.view[i][j]);
+        printf("\n");
+    }
+}
+
+struct point get_point(int col, char row)
+{
+    struct point ret_point;
+    ret_point.row = row;
+    ret_point.col = col;
+    return ret_point;
+};
+
+int get_lenght(struct Ship ship)
+{
+    int dcol = ship.top.col - ship.back.col;
+    int drow = ship.top.row - ship.back.row;
+    if(dcol < 0)
+        dcol = -dcol;
+    if(drow < 0)
+        drow = -drow;
+    int res = (dcol + 1) * (drow + 1);
+    return res;
 }
